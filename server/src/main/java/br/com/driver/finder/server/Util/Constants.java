2ce50f8ba374bd0641e81f9a3f98e7c5002a7134
Artur Branco <arturfdb@gmail.com>
2022-09-22 23:17:17 -0300

    feat(Constants): add constants for all routes.

--- server/src/main/java/br/com/driver/finder/server/Util/Constants.java
+++ server/src/main/java/br/com/driver/finder/server/Util/Constants.java
@@ -3,4 +3,11 @@ package br.com.driver.finder.server.Util;
 public class Constants {
     public static final String GEOLOCATION_URL = "https://www.googleapis.com/geolocation/v1/geolocate";
     public static final String GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json";
+    public static final String REGISTER_DRIVER_ROUTE = "registerDriver";
+    public static final String FIND_DRIVER_ROUTE = "findDriver";
+    public static final String SEND_MESSAGE_ROUTE = "sendMessage";
+    public static final String END_RIDE_ROUTE = "endRide";
+    public static final String PASSENGER_CONNECTION_REQUEST = "passengerConnection";
+    public static final String RECEIVE_MESSAGE_REQUEST = "receiveMessage";
+    public static final String RIDE_FINISHED_REQUEST = "rideFinished";
 }

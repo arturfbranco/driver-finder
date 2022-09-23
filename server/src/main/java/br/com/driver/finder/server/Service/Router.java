a20d8e61579f6f40d4d7932ba1c88d6ee5f971c2
Artur Branco <arturfdb@gmail.com>
2022-09-22 23:14:45 -0300

    feat(Router): implement router to services.

--- server/src/main/java/br/com/driver/finder/server/Service/Router.java
+++ server/src/main/java/br/com/driver/finder/server/Service/Router.java
@@ -1,9 +1,41 @@
 package br.com.driver.finder.server.Service;
 
+import br.com.driver.finder.server.Util.Constants;
 import org.json.JSONObject;
 
 public class Router {
+
+    private final DriverService driverService;
+    private final PassengerService passengerService;
+    private final MessageService messageService;
+    private final FinalizerService finalizerService;
+
+    public Router(){
+        this.driverService = new DriverService();
+        this.passengerService = new PassengerService();
+        this.messageService = new MessageService();
+        this.finalizerService = new FinalizerService();
+    }
     public JSONObject callService(JSONObject request){
-        return new AddressService().buildAddress(request.getString("address"));
+        if(request.getString("status").equals("400")){
+            return request;
+        }
+        return this.routeRequest(request);
+    }
+    private JSONObject routeRequest(JSONObject jsonObject){
+        switch (jsonObject.getString("route")){
+            case Constants.REGISTER_DRIVER_ROUTE:
+                return this.driverService.registerDriver(jsonObject);
+            case Constants.FIND_DRIVER_ROUTE:
+                return this.passengerService.findDriver(jsonObject);
+            case Constants.SEND_MESSAGE_ROUTE:
+                return this.messageService.sendMessage(jsonObject);
+            case Constants.END_RIDE_ROUTE:
+                return this.finalizerService.finalizeRide(jsonObject);
+            default:
+                JSONObject errorJson = new JSONObject();
+                errorJson.put("status", "400");
+                return errorJson;
+        }
     }
 }
